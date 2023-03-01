b7fe6d6ad9454adae3c6a472398c300bae8fd6dd
ThanhTran2k1 <tranquocthanhd19ptiter@gmail.com>
2023-03-01 13:57:37 +0700

    RunUpdate

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <Arduino.h>
-
 #include <DMDESP.h>
-#include <fonts/ElektronMart6x8.h>
-#include <fonts/EMSans5x6.h>
+// #include <fonts/ElektronMart6x8.h>
+#include <fonts/DejaVuSansItalic9.h>
 //----------------------------------------
 
 //----------------------------------------DMD Configuration (P10 Panel)
@@ -16,7 +15,7 @@ void Scrolling_Text(int y, uint8_t scrolling_speed) {
   static uint32_t pM;
   static uint32_t x;
   int width = Disp.width();
-  Disp.setFont(EMSans5x6);
+  Disp.setFont(DejaVuSansItalic9);
   int fullScroll = Disp.textWidth(Text[0]) + width;
   if((millis() - pM) > scrolling_speed) { 
     pM = millis();
@@ -34,7 +33,7 @@ void setup() {
   //----------------------------------------DMDESP Setup
   Disp.start(); //--> Run the DMDESP library
   Disp.setBrightness(100); //--> Brightness level
-  Disp.setFont(EMSans5x6); //--> Determine the font used
+  Disp.setFont(DejaVuSansItalic9); //--> Determine the font used
   //----------------------------------------
 }
 //========================================================================
@@ -42,9 +41,9 @@ void setup() {
 //========================================================================VOID LOOP()
 void loop() {
   Disp.loop(); //--> Run "Disp.loop" to refresh the LED
-  Disp.drawText(0, 0, "1234567"); //--> Display text "Disp.drawText(x position, y position, text)"
+  Disp.drawText(0, -1, "1234567"); //--> Display text "Disp.drawText(x position, y position, text)"
   // Disp.drawText(0,9,"122334455");
- Scrolling_Text(9, 60); //--> Show running text "Scrolling_Text(y position, speed);"
+ Scrolling_Text(8, 60); //--> Show running text "Scrolling_Text(y position, speed);"
 }
 //========================================================================
 
