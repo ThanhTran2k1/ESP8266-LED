f6226af0667b2484500e471266550a4754a4d506
ThanhTran2k1 <tranquocthanhd19ptiter@gmail.com>
2023-03-07 11:31:19 +0700

    TranThanh/updateMQTT
    Đã kết nối thành công MQTT qua scheduler. Đã nhận được msg từ inTopic và outTopic

--- src/main.cpp
+++ src/main.cpp
@@ -1,128 +1,117 @@
 #include <Arduino.h>
-#include <DMDESP.h>
-// #include <fonts/ElektronMart6x8.h>
-#include <fonts/DejaVuSansItalic9.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <TaskScheduler.h>
-
+#include <DMDESP.h>
+Scheduler runner;
+// Callback methods prototypes
+void setupWiFi();
+void t2Callback();
+void t3Callback(); 
+void connectMQTTCallback();
+// Tasks
+Task t4();
+Task t1(2000, TASK_ONCE, &setupWiFi, &runner, true);  //adding task to the chain on creation
+Task t2(2000, TASK_FOREVER, &t2Callback, &runner, true);  //adding task to the chain on creation
+Task t3(5000, TASK_FOREVER, &t3Callback);
+Task connectMQTT(2000, TASK_ONCE, &connectMQTTCallback, &runner, true);
+//wifi
 const char* ssid = "AIoT JSC";
 const char* password = "aiot1234@";
-
-const int mqtt_port = 1883;
-const char* mqtt_topic = "test";
+//mqtt server
+const char* mqtt_server = "192.168.1.133";
+const uint16_t mqtt_port = 1883;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-Scheduler runner;
-
-void connectWiFiCallback() {
+void callback(char* topic, byte* payload, int length) 
+{
+  Serial.print("Co tin nhan moi tu topic:");
+  Serial.println(topic);
+  for (int i = 0; i < length; i++) 
+  Serial.print((char)payload[i]);
   Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+}
 
+void connectMQTTCallback() {
+  client.setServer(mqtt_server,mqtt_port);
+  client.setCallback(callback);
+}
+
+
+void setupWiFi() {
+  Serial.println();
+  Serial.print("Đang kết nối tới ");
+  Serial.println(ssid);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
   randomSeed(micros());
-
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  connectMQTT.enable();
+}
+
+void t2Callback() {
+      Serial.print("t2: ");
+      Serial.println(millis());
   
 }
 
-Task connectWiFi(500,TASK_FOREVER,&connectWiFiCallback,&runner);
-
-
-
-
-// void connect() {
-//   // Kết nối Wifi
-//   WiFi.begin(ssid, password);
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(1000);
-//   }
+void t3Callback() {
+    Serial.print("t3: ");
+    Serial.println(millis());
   
-//   // Kết nối MQTT
-//   while (!client.connected()) {
-//     if (client.connect("ESP8266Client")) {
-//       Serial.println("MQTT connected");
-//       client.subscribe(mqtt_topic);
-//     } else {
-//       delay(1000);
-//     }
-//   }
-// }
-
-void setup() {
-  // runner.startNow();
-  connectWiFi.enable();
-
 }
 
-void loop(){
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Đang kết nối tới MQTT.....");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("Đã kết nối!");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("Kết nối MQTT thất bại, rc=");
+      Serial.print(client.state());
+      Serial.println(" Thử lại sau 5s");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+  Serial.println("Da ket noi MQTT thanh cong !");
+}
+
+
+void setup () {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Scheduler TEST");
+  
+  runner.startNow();  // set point-in-time for scheduling start
+}
+
+
+void loop () {
+  
   runner.execute();
+  if(!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+//  Serial.println("Loop ticks at: ");
+//  Serial.println(millis());
 }
-
-
-
-//----------------------------------------
-/**
-A	D0
-B	D6
-CLK	D5
-SCK	D3
-R	D7
-NOE	D8
-GND	GND
-*/
-//----------------------------------------DMD Configuration (P10 Panel)
-// #define DISPLAYS_WIDE 1 //--> Panel Columns
-// #define DISPLAYS_HIGH 1 //--> Panel Rows
-// DMDESP Disp(DISPLAYS_WIDE, DISPLAYS_HIGH);  //--> Number of Panels P10 used (Column, Row)
-// //----------------------------------------
-// static char *Text[] = {"123456789--"};
-
-// void Scrolling_Text(int y, uint8_t scrolling_speed) {
-//   static uint32_t pM;
-//   static uint32_t x;
-//   int width = Disp.width();
-//   Disp.setFont(DejaVuSansItalic9);
-//   int fullScroll = Disp.textWidth(Text[0]) + width;
-//   if((millis() - pM) > scrolling_speed) { 
-//     pM = millis();
-//     if (x < fullScroll) {
-//       ++x;
-//     } else {
-//       x = 0;
-//       return;
-//     }
-//     Disp.drawText(width - x, y, Text[0]);
-//   }  
-// }
-//========================================================================VOID SETUP()
-// void setup() {
-//   //----------------------------------------DMDESP Setup
-//   Disp.start(); //--> Run the DMDESP library
-//   Disp.setBrightness(100); //--> Brightness level
-//   Disp.setFont(DejaVuSansItalic9); //--> Determine the font used
-//   //----------------------------------------
-// }
-// //========================================================================
-
-// //========================================================================VOID LOOP()
-// void loop() {
-//   Disp.loop(); //--> Run "Disp.loop" to refresh the LED
-//   Disp.drawText(0, -1, "1234567"); //--> Display text "Disp.drawText(x position, y position, text)"
-//   // Disp.drawText(0,9,"122334455");
-//  Scrolling_Text(8, 60); //--> Show running text "Scrolling_Text(y position, speed);"
-// }
-
