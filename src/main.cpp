be4827c6faf688794b2fdb9ff628bb7ba5242186
ThanhTran2k1 <tranquocthanhd19ptiter@gmail.com>
2023-03-09 12:00:57 +0700

    TranThanh/Loi-ket-noi
    Không thể kết nối MQTT. lỗi rc=-2. Chưa tìm ra cách fix để chạy.

--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,8 @@
 
 // Smart Config WiFi
 int statusCode;
-const char* ssidWF = "AIoT Tang 1";
-const char* passWF = "aiot1234@";
+const char* ssidWF = "text";
+const char* passWF = "text";
 String st;
 String content;
 
@@ -31,9 +31,11 @@ String content;
 bool testWifi(void);
 void launchWeb(void);
 void setupAP(void);
-
+void configWifi(void);
+void readEEPROM(void);
 // Thiet lap server Local tai cong 80 moi khi duoc yeu cau
 ESP8266WebServer server(80);
+
 
 
 
@@ -54,10 +56,12 @@ Scheduler runner; // tạo con trỏ để chạy Scheduler
 
 void connectionWiFi(); 
 void connectMQTTCallback();
+void keepAliveCallback();
+void t2Callback();
 // Tasks
-Task task_connectWifi(2000, TASK_ONCE, &connectionWiFi, &runner, true);  //adding task to the chain on creation
-// Task t2(0.2, TASK_FOREVER, &t2Callback, &runner, true);  //adding task to the chain on creation
-Task task_connectMQTT(2000, TASK_ONCE, &connectMQTTCallback, &runner, true);
+Task task_connectWifi(2000, TASK_FOREVER, &connectionWiFi, &runner, true);  //adding task to the chain on creation
+Task task_connectMQTT(2000, TASK_ONCE, &connectMQTTCallback, NULL, false);
+Task t2(0.2, TASK_FOREVER, &t2Callback, &runner, true);  //adding task to the chain on creation
 //wifi
 // const char* ssid = "AIoT Tang 1"; // tên WIFI
 // const char* password = "aiot1234@"; // Mật khẩu WIFI
@@ -112,26 +116,210 @@ void connectMQTTCallback() {
 
 }
 
-// Kết nối WIFI. 
-void connectionWiFi() {
-  Serial.println();
-  Serial.print("Đang kết nối tới ");
-  Serial.println(ssidWF);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssidWF, passWF);
-  while (WiFi.status() != WL_CONNECTED) {
+bool testWifi(void) {
+
+  int count = 0;
+  Serial.println("Doi wifi de ket noi");
+  while(count<20) {
+    if(WiFi.status() == WL_CONNECTED) {
+      return true;
+    }
     delay(500);
-    Serial.print("..");
+    Serial.print("*");
+    count++;
   }
-  Serial.println("");
-  Serial.println("Đã kết nối WiFi !");
-  Serial.println("Địa chỉ IP: ");
-  Serial.println(WiFi.localIP());
-  task_connectMQTT.enable(); // sau khi kết nối WIFI thì kết nối MQTT để nhận dữ liệu
+  Serial.println("Ket noi WiFi that bai !");
+  return false;
 }
 
-// void t2Callback() {
-// }
+void readEEPROM() {
+  Serial.println("Dang doc SSID va PASS tu EEPROM ");
+  String esid;
+  for(int i=0;i<32;i++) {
+    esid += char(EEPROM.read(i));
+  }
+  String epass = "";
+  for(int i=32;i<96;i++) {
+    epass += char(EEPROM.read(i));
+  }
+  Serial.print("SSID: ");
+  Serial.println(esid);
+  Serial.print("PASS: ");
+  Serial.println(epass);
+  ssidWF = esid.c_str();
+  passWF = epass.c_str();
+  WiFi.begin(ssidWF,passWF);
+  
+}
+
+void setupAP(void)
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  st = "<ol>";
+  for (int i = 0; i < n; ++i)
+  {
+    // Print SSID and RSSI for each network found
+    st += "<li>";
+    st += WiFi.SSID(i);
+    st += " (";
+    st += WiFi.RSSI(i);
+ 
+    st += ")";
+    st += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
+    st += "</li>";
+  }
+  st += "</ol>";
+  delay(100);
+  WiFi.softAP("how2electronics", "");
+  Serial.println("softap");
+  launchWeb();
+  Serial.println("over");
+}
+
+// Kết nối WIFI. 
+void connectionWiFi() {
+  readEEPROM();
+  if(testWifi()) {
+    Serial.println("Ket noi WiFi thanh cong !");
+    task_connectMQTT.enable(); // sau khi kết nối WIFI thì kết nối MQTT để nhận dữ liệu
+  }else {
+    Serial.println("bat che do HostPot");
+    launchWeb();
+    setupAP();
+  }
+  Serial.println();
+  Serial.println("Waiting.");
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+    server.handleClient();
+  }
+  // Serial.println();
+  // Serial.print("Đang kết nối tới ");
+  // Serial.println(ssidWF);
+  // WiFi.mode(WIFI_STA);
+  // WiFi.begin(ssidWF, passWF);
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print("..");
+  // }
+  // Serial.println("");
+  // Serial.println("Đã kết nối WiFi !");
+  // Serial.println("Địa chỉ IP: ");
+  // Serial.println(WiFi.localIP());
+  
+}
+
+void createWebServer()
+{
+ {
+    server.on("/", []() {
+ 
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+      content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
+      content += "<form action=\"/scan\" method=\"POST\"><input type=\"submit\" value=\"scan\"></form>";
+      content += ipStr;
+      content += "<p>";
+      content += st;
+      content += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+      content += "</html>";
+      server.send(200, "text/html", content);
+    });
+    server.on("/scan", []() {
+      //setupAP();
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+ 
+      content = "<!DOCTYPE HTML>\r\n<html>go back";
+      server.send(200, "text/html", content);
+    });
+ 
+    server.on("/setting", []() {
+      String qsid = server.arg("ssid");
+      String qpass = server.arg("pass");
+      if (qsid.length() > 0 && qpass.length() > 0) {
+        Serial.println("clearing eeprom");
+        for (int i = 0; i < 96; ++i) {
+          EEPROM.write(i, 0);
+        }
+        Serial.println(qsid);
+        Serial.println("");
+        Serial.println(qpass);
+        Serial.println("");
+ 
+        Serial.println("writing eeprom ssid:");
+        for (int i = 0; i < qsid.length(); ++i)
+        {
+          EEPROM.write(i, qsid[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qsid[i]);
+        }
+        Serial.println("writing eeprom pass:");
+        for (int i = 0; i < qpass.length(); ++i)
+        {
+          EEPROM.write(32 + i, qpass[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qpass[i]);
+        }
+        EEPROM.commit();
+ 
+        content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+        statusCode = 200;
+        ESP.reset();
+      } else {
+        content = "{\"Error\":\"404 not found\"}";
+        statusCode = 404;
+        Serial.println("Sending 404");
+      }
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.send(statusCode, "application/json", content);
+ 
+    });
+  } 
+}
+
+void launchWeb()
+{
+  Serial.println("");
+  if (WiFi.status() == WL_CONNECTED)
+    Serial.println("WiFi connected");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("SoftAP IP: ");
+  Serial.println(WiFi.softAPIP());
+  createWebServer();
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+}
+
+void t2Callback() {
+}
 
 // Nếu mất kết nối thì reconnect
 void reconnect() {
@@ -187,11 +375,13 @@ void Scrolling_Text(int y, uint8_t scrolling_speed) {
 
 void setup () {
   Serial.begin(115200);
-  delay(1000);
-  Serial.println("Start");
+  Serial.println("Ngat ket noi WiFi hien tai !");
+  WiFi.disconnect();
+  EEPROM.begin(512); // khoi tao EEPROM de luu WIFI
   Disp.start(); // Khởi tạo hàm hiển thị
   Disp.setBrightness(100); // Thay đổi độ sáng 
-  runner.startNow();  // Khởi tạo bộ lên lịch Scheduler và chạy bằng con trỏ runner
+  runner.addTask(task_connectWifi);
+  // runner.startNow();  // Khởi tạo bộ lên lịch Scheduler và chạy bằng con trỏ runner
 }
 
 
